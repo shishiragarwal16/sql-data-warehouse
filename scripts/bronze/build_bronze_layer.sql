3e3e17766f4fc1de4bd533710f28bf160091f866
shishiragarwal16 <114521464+shishiragarwal16@users.noreply.github.com>
2025-09-06 16:35:21 +0530

    updtd

---
+++ scripts/bronze/build_bronze_layer.sql
@@ -0,0 +1,8 @@
+EXEC bronze.load_bronze;
+
+select count(*) from bronze.crm_cust_info
+select count(*) from bronze.crm_prd_info
+select count(*) from bronze.crm_sales_details
+select count(*) from bronze.erp_cust_az12
+select count(*) from bronze.erp_loc_a101
+select count(*) from bronze.erp_px_cat_g1v2
